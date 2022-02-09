58b009636faec3ed85b6a7ed88690ec30cd88ef2
Youssef <youssefra3@gmail.com>
2022-02-09 11:08:10 +0100

    base de datos actualizada

--- OTROS/atm.sql
+++ OTROS/atm.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-02-2022 a las 11:24:55
+-- Tiempo de generación: 09-02-2022 a las 11:06:02
 -- Versión del servidor: 10.4.22-MariaDB
 -- Versión de PHP: 7.4.27
 
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `clientes` (
-  `ID` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `nombre` varchar(50) NOT NULL,
   `apellidos` varchar(100) NOT NULL,
   `f_nacimiento` date NOT NULL,
@@ -36,18 +36,18 @@ CREATE TABLE `clientes` (
   `direccion` varchar(100) NOT NULL,
   `poblacion` varchar(100) NOT NULL,
   `usuario` varchar(45) DEFAULT NULL,
-  `contrasena` varchar(100) DEFAULT NULL
+  `contrasena` varchar(100) DEFAULT NULL,
+  `f_cr` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Volcado de datos para la tabla `clientes`
 --
 
-INSERT INTO `clientes` (`ID`, `nombre`, `apellidos`, `f_nacimiento`, `dni`, `direccion`, `poblacion`, `usuario`, `contrasena`) VALUES
-(1, 'youssef', 'rahou', '0000-00-00', 'x75845934', 'calle mayor, 65', 'manacor', 'youssef', '12345678'),
-(2, 'aaaaaa', 'aaaaaa', '0000-00-00', 'aaaaaa', 'aaaaaa', 'aaaaaa', 'aaaaaa', 'aaaaaa'),
-(3, 'ino', 'nion', '0000-00-00', 'oi', 'nio', 'io', 'i', 'i'),
-(4, 'inomk', 'oimk', '0000-00-00', 'niom', 'nom', 'nom', 'om,l´ñ.k', 'om,l´ñ.k');
+INSERT INTO `clientes` (`id`, `nombre`, `apellidos`, `f_nacimiento`, `dni`, `direccion`, `poblacion`, `usuario`, `contrasena`, `f_cr`) VALUES
+(6, 'yuijo', 'uio', '1998-12-05', 'uijok', 'guyi', 'vubin', 'buin@ewg.r', 'hola', '2022-02-04 13:29:23'),
+(8, 'hgjk', 'hbjnkl', '1999-01-01', 'jkml', 'ui', 'uinjmo', 'hbnj', '4d186321c1a7f0f354b297e8914ab240', '2022-02-04 13:29:23'),
+(9, 'Fran', 'Duque', '1963-01-13', '3043535X', 'calle del sol', '07020', 'fran@casa.com', 'pepona', '2022-02-04 13:29:23');
 
 -- --------------------------------------------------------
 
@@ -56,10 +56,9 @@ INSERT INTO `clientes` (`ID`, `nombre`, `apellidos`, `f_nacimiento`, `dni`, `dir
 --
 
 CREATE TABLE `conexiones` (
-  `idconexiones` int(11) NOT NULL,
-  `time` timestamp NULL DEFAULT NULL,
-  `conexionescol` varchar(45) DEFAULT NULL,
-  `clientes_ID` int(11) NOT NULL
+  `id` int(11) NOT NULL,
+  `f_hora` timestamp NULL DEFAULT NULL,
+  `id_cliente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -72,7 +71,7 @@ CREATE TABLE `cuentas_corrientes` (
   `id` int(11) NOT NULL,
   `iban` varchar(20) NOT NULL,
   `balance` double NOT NULL,
-  `clientes_ID` int(11) NOT NULL
+  `id_cliente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -86,7 +85,7 @@ CREATE TABLE `tarjetas` (
   `numero_tarjeta` int(16) NOT NULL,
   `fecha_coducidad` date NOT NULL,
   `cvv` int(3) NOT NULL,
-  `cuentas_corrientes_id` int(11) NOT NULL
+  `id_cuenta_corriente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
@@ -97,10 +96,11 @@ CREATE TABLE `tarjetas` (
 
 CREATE TABLE `transacciones` (
   `id` int(11) NOT NULL,
-  `tipo` int(11) NOT NULL,
-  `cantidad` double NOT NULL,
-  `tarjetas_id` int(11) DEFAULT NULL,
-  `cuentas_corrientes_id` int(11) DEFAULT NULL
+  `tipo_transaccion` tinyint(1) NOT NULL COMMENT '0 sacar, 1 meter dinero',
+  `cantidad_transaccion` double NOT NULL,
+  `id_tarjeta` int(11) DEFAULT NULL,
+  `id_cuenta_corriente` int(11) DEFAULT NULL,
+  `id_cliente` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -111,36 +111,36 @@ CREATE TABLE `transacciones` (
 -- Indices de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  ADD PRIMARY KEY (`ID`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indices de la tabla `conexiones`
 --
 ALTER TABLE `conexiones`
-  ADD PRIMARY KEY (`idconexiones`),
-  ADD KEY `fk_conexiones_clientes1_idx` (`clientes_ID`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_conexiones_clientes1_idx` (`id_cliente`);
 
 --
 -- Indices de la tabla `cuentas_corrientes`
 --
 ALTER TABLE `cuentas_corrientes`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_cuentas_corrientes_clientes_idx` (`clientes_ID`);
+  ADD KEY `fk_cuentas_corrientes_clientes_idx` (`id_cliente`);
 
 --
 -- Indices de la tabla `tarjetas`
 --
 ALTER TABLE `tarjetas`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_tarjetas_cuentas_corrientes1_idx` (`cuentas_corrientes_id`);
+  ADD KEY `fk_tarjetas_cuentas_corrientes1_idx` (`id_cuenta_corriente`);
 
 --
 -- Indices de la tabla `transacciones`
 --
 ALTER TABLE `transacciones`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `fk_transacciones_tarjetas1_idx` (`tarjetas_id`),
-  ADD KEY `fk_transacciones_cuentas_corrientes1_idx` (`cuentas_corrientes_id`);
+  ADD KEY `fk_transacciones_tarjetas1_idx` (`id_tarjeta`),
+  ADD KEY `fk_transacciones_cuentas_corrientes1_idx` (`id_cuenta_corriente`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -150,13 +150,13 @@ ALTER TABLE `transacciones`
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `conexiones`
 --
 ALTER TABLE `conexiones`
-  MODIFY `idconexiones` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `cuentas_corrientes`
@@ -184,26 +184,26 @@ ALTER TABLE `transacciones`
 -- Filtros para la tabla `conexiones`
 --
 ALTER TABLE `conexiones`
-  ADD CONSTRAINT `fk_conexiones_clientes1` FOREIGN KEY (`clientes_ID`) REFERENCES `clientes` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_conexiones_clientes1` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `cuentas_corrientes`
 --
 ALTER TABLE `cuentas_corrientes`
-  ADD CONSTRAINT `fk_cuentas_corrientes_clientes` FOREIGN KEY (`clientes_ID`) REFERENCES `clientes` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_cuentas_corrientes_clientes` FOREIGN KEY (`id_cliente`) REFERENCES `clientes` (`ID`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `tarjetas`
 --
 ALTER TABLE `tarjetas`
-  ADD CONSTRAINT `fk_tarjetas_cuentas_corrientes1` FOREIGN KEY (`cuentas_corrientes_id`) REFERENCES `cuentas_corrientes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_tarjetas_cuentas_corrientes1` FOREIGN KEY (`id_cuenta_corriente`) REFERENCES `cuentas_corrientes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 
 --
 -- Filtros para la tabla `transacciones`
 --
 ALTER TABLE `transacciones`
-  ADD CONSTRAINT `fk_transacciones_cuentas_corrientes1` FOREIGN KEY (`cuentas_corrientes_id`) REFERENCES `cuentas_corrientes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  ADD CONSTRAINT `fk_transacciones_tarjetas1` FOREIGN KEY (`tarjetas_id`) REFERENCES `tarjetas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+  ADD CONSTRAINT `fk_transacciones_cuentas_corrientes1` FOREIGN KEY (`id_cuenta_corriente`) REFERENCES `cuentas_corrientes` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `fk_transacciones_tarjetas1` FOREIGN KEY (`id_tarjeta`) REFERENCES `tarjetas` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
